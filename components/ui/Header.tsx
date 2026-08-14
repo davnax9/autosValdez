7322b64ffce3a96b3ba51b9a8a1bdf0367fa233a
DAVID VALDEZ ZAVALA <dzavala@paquetexpress.com.mx>
2026-08-14 12:35:42 -0700

    Actualiza redes sociales

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -25,11 +25,11 @@ export default function Header() {
         </div>
 
         <div className="flex justify-center md:justify-end gap-3">
-          <a href="https://www.facebook.com" target="_blank">
+          <a href="https://www.facebook.com/luismartin.valdezhernandez" target="_blank">
             <Image src="/facebook.png" alt="Facebook" width={30} height={30}/>
           </a>
 
-          <a href="https://wa.me/526681234567" target="_blank">
+          <a href="https://wa.me/526681013568" target="_blank">
             <Image src="/whatsapp.png" alt="WhatsApp" width={30} height={30}/>
           </a>
         </div>
--- components/ui/RedesSociales.tsx
+++ components/ui/RedesSociales.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function RedesSociales() {
   return (
     <>
-        <a href="https://www.facebook.com"target="_blank">
+        <a href="https://www.facebook.com/luismartin.valdezhernandez"target="_blank">
             <Image src="/facebook.png" alt="Facebook" width={30} height={30}/>
         </a>
 
-        <a href="https://wa.me/526681234567" target="_blank">
+        <a href="https://wa.me/526681013568" target="_blank">
             <Image src="/whatsapp.png" alt="WhatsApp" width={30} height={30}/>
         </a>
     </>
